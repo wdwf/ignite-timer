172db3516c35d11b0719f5b9679f54f8e8c31d53
wdwf <w_dwf@hotmail.com>
2025-02-19 17:18:11 -0300

    :lipstick: feat: add styled-components theme e prove global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
